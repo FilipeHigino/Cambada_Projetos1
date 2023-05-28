522ef6f789a13589761433ef7e9e5cd1f7fac623
Filipe <104517794+FilipeHigino@users.noreply.github.com>
2023-05-28 17:25:11 -0300

    alimentacao (sem tratamento)

--- alimentar.py
+++ alimentar.py
@@ -7,39 +7,48 @@ def alimentar():
     total = 100
     while True:
         time.sleep(2)
-        decorrer = total - 5
+        decorrer = total - 25
         if decorrer<total:
             total = decorrer
+            if total >=100:
+                total = 100
+                decorrer = total
         print(total)
-        print("Aumentando fome...")
-        if total == 75:
-            print("\nComeçando a ficar com fome...")
-            alimentar = int(input("Deseja alimentar o bichinho? [0 = NÃO e 1 = SIM]: "))
-            if alimentar == 1:
-                print("Huuum comida.")
-                total+=25
-            else:
-                continue
-        elif total == 50:
-            print("\nBicho tem fome.")
-            alimentar = int(input("Deseja alimentar o bichinho? [0 = NÃO e 1 = SIM]: "))
-            if alimentar == 1:
-                print("Huuum comida.")
-                total + 50
-            else:
-                continue
-        elif total == 25:
-            print("\nME ALIMENTE!")
-            alimentar = int(input("Deseja alimentar o bichinho? [0 = NÃO e 1 = SIM]: "))
-            if alimentar == 1:
-                print("Huuum comida.")
-                total + 75
-            else:
-                continue
+        alimentar = int(input("Aumentando fome... Alimentar? [0 - NÃO / 1 - SIM]:  "))
+        if alimentar == 1:
+            opcao = int(input("""Digite a opção desejada:
+                    [1] - Cuscuz (+10)
+                    [2] - Tapioca (+25)
+                    [3] - Bolo de Rolo (+50)    
+                """))
+            if opcao == 1:
+                print("Cuscuz diminui 10 de fome")
+                cuscuz = int(input("Deseja dar Cuscuz? [0 - NÃO / 1 - SIM]: "))
+                if cuscuz== 1:
+                    total+=10
+                else:
+                    continue
+            elif opcao == 2:
+                print("Tapioca diminui 25 de fome")
+                tapioca = int(input("Deseja dar Tapioca? [0 - NÃO / 1 - SIM]: "))
+                if tapioca == 1:
+                    total+=25
+                else:
+                    continue
+            elif opcao == 3:
+                print("Bolo de rolo diminui 50 de fome")
+                bolo = int(input("Deseja dar cuscuz? [0 - NÃO / 1 - SIM]: "))
+                if bolo == 1:
+                    total+=50
+                else:
+                    continue
+        else:
+           print("Nao alimentando...")
+           continue
         if total == 0:
             print("\nSem... Forças....")
             time.sleep(2)
-            print("\nMude o mundo, minha utlima mensagem... Adeus.")
+            print("\nMude o mundo, minha ultima mensagem... Adeus.")
             time.sleep(2)
             print("\nBichinho morreu.")
             break
