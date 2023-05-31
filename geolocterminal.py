33073079a9dd9ed8fd5bec947e7522314a64a0ca
lucasrosati <101799804+lucasrosati@users.noreply.github.com>
2023-05-31 08:19:21 -0300

    Tratamento de erros e exceções GEOLOC.py

--- geolocterminal.py
+++ geolocterminal.py
@@ -1,11 +1,13 @@
 import geocoder
 
 def solicitar_localizacao():
-    resposta = input("Deseja permitir acesso à sua localização? (sim/não): ")
-    if resposta.lower() == "sim":
-        g = geocoder.ip('me')
-        print("Sua localização é: ", g.latlng)
-    else:
-        print("Acesso à localização negado.")
+        resposta = input("Deseja permitir acesso à sua localização? (sim/não): ")
+        if resposta.lower() == "sim":
+            g = geocoder.ip('me')
+            print("Sua localização é: ", g.latlng)
+        elif resposta.lower() == "não" or resposta.lower() == "nao":
+            print("Acesso à localização negado.")
+        else:
+            print("Ocorreu um erro durante a solicitação da localização:")
 
 solicitar_localizacao()
